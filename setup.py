2892d3d24d1e8a992ac57f5ca68de9b9f025d823
Gerald Wuersching <gerald.wuersching@tum.de>
2022-04-12 15:54:50 +0200

    setup.py CMake extension works

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 import os
 import sys
 import subprocess
+import warnings
+import platform
+import pathlib
+import glob
 
 from commonroad_reach.__version__ import __version__
 from setuptools import setup, find_packages, Extension
@@ -27,6 +31,7 @@ class CMakeBuild(build_ext):
                                ", ".join(e.name for e in self.extensions))
 
         for ext in self.extensions:
+            print(ext)
             self.build_extension(ext)
 
     def build_extension(self, ext):
@@ -50,7 +55,15 @@ class CMakeBuild(build_ext):
             "-DPYTHON_EXECUTABLE=" + python_executable,
         ]
 
-        # TODO: add CRDC_DIR and PYTHON_VER
+        # get path the drivability checker root
+        if 'CRDC_DIR' in os.environ:
+            cmake_args += ['-DCRDC_DIR=' + os.environ['CRDC_DIR']]
+        else:
+            warnings.warn('\t\t\t Please specify the path to the drivability checker root folder!')
+            return None
+
+        # pass version of current python binary
+        cmake_args += ['-DPYTHON_VER=' + platform.python_version()[0:3]]
 
         # add tests
         if 'ADD_TESTS' in os.environ:
@@ -61,9 +74,9 @@ class CMakeBuild(build_ext):
         # CMake build type (debug / release)
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
-
         cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
 
+        # number of Build Jobs
         if 'BUILD_JOBS' in os.environ:
             build_args += ['--'] + ['-j'] + [os.environ['BUILD_JOBS']]
 
@@ -73,11 +86,17 @@ class CMakeBuild(build_ext):
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
 
+        # build
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=build_dir)
         subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=build_dir)
 
+        # copy files
+        install_dir = self.get_ext_fullpath(ext.name)
+        extension_install_dir = pathlib.Path(install_dir).parent.joinpath(ext.name).resolve()
+        for file in glob.glob((f'{pathlib.Path(build_dir).parent.resolve()}/' + 'pycrreach.*.so')):
+            self.copy_file(file, extension_install_dir)
 
-# TODO: does not build/install C++ code
+
 setup(name='commonroad-reach', version=__version__,
       description='CommonRoad-Reach: A Toolbox for Computing Reachable Sets of Automated Vehicles',
       keywords='autonomous automated vehicles driving motion planning',
@@ -91,6 +110,13 @@ setup(name='commonroad-reach', version=__version__,
       author_email='commonroad@lists.lrz.de',
       license="BSD",
       packages=find_packages(exclude=['tests']),
+
+      ext_modules=[
+          CMakeExtension("commonroad_reach"),
+          # cythonize("commonroad_reach/utility/util_py_grid_online_reach.pyx", build_dir="build-cython")
+      ],
+      cmdclass={"build_ext": CMakeBuild},
+
       install_requires=["commonroad-io>=2021.3",
                         "commonroad-route-planner>=2022.1",
                         "omegaconf>=2.1.1",
@@ -111,8 +137,4 @@ setup(name='commonroad-reach', version=__version__,
                    "Operating System :: POSIX :: Linux",],
       data_files=[],
       include_package_data=True,
-      ext_modules=[
-          CMakeExtension("commonroad_reach"),
-          cythonize("commonroad_reach/utility/util_py_grid_online_reach.pyx", build_dir="build-cython")
-      ]
       )
