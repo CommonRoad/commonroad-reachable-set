fix minor issue in online/offline computation

commonroad_reach/data_structure/configuration.py
@@ -381,38 +381,28 @@ class PlanningConfiguration:
             self.v_lon_initial, self.v_lat_initial = v_initial
 
     @property
-    def p_lon_lat_initial(self):
+    def p_initial(self):
         return np.array([self.p_lon_initial, self.p_lat_initial])
 
-    @p_lon_lat_initial.setter
-    def p_lon_lat_initial(self, p_lon_lat_initial: Tuple):
-        """
-        :param p_lon_lat_initial: Tuple of length 2 with
-                                  p_lon_lat_initial[0]: longitudinal position
-                                  p_lon_lat_initial[1]: lateral position
-        """
-        assert (type(p_lon_lat_initial is tuple)), "Initial lon/lat position must be of type tuple with length 2"
-        assert (len(p_lon_lat_initial) == 2), "Initial lon/lat position must be of type tuple with length 2"
+    @p_initial.setter
+    def p_initial(self, p_initial: Tuple):
+        assert (type(p_initial is tuple)), "Initial lon/lat position must be of type tuple with length 2"
+        assert (len(p_initial) == 2), "Initial lon/lat position must be of type tuple with length 2"
 
-        self.p_lon_initial = p_lon_lat_initial[0]
-        self.p_lat_initial = p_lon_lat_initial[1]
+        self.p_lon_initial = p_initial[0]
+        self.p_lat_initial = p_initial[1]
 
     @property
-    def v_lon_lat_initial(self):
+    def v_initial(self):
         return np.array([self.v_lon_initial, self.v_lat_initial])
 
-    @v_lon_lat_initial.setter
-    def v_lon_lat_initial(self, v_lon_lat_initial: Tuple):
-        """
-        :param v_lon_lat_initial: Tuple of length 2 with
-                                  v_lon_lat_initial[0]: longitudinal velocity
-                                  v_lon_lat_initial[1]: lateral velocity
-        """
-        assert (type(v_lon_lat_initial is tuple)), "Initial lon/lat velocity must be of type tuple with length 2"
-        assert (len(v_lon_lat_initial) == 2), "Initial lon/lat velocity must be of type tuple with length 2"
+    @v_initial.setter
+    def v_initial(self, v_initial: Tuple):
+        assert (type(v_initial is tuple)), "Initial lon/lat velocity must be of type tuple with length 2"
+        assert (len(v_initial) == 2), "Initial lon/lat velocity must be of type tuple with length 2"
 
-        self.v_lon_initial = v_lon_lat_initial[0]
-        self.v_lat_initial = v_lon_lat_initial[1]
+        self.v_lon_initial = v_initial[0]
+        self.v_lat_initial = v_initial[1]
 
 
 class ReachableSetConfiguration:

commonroad_reach/data_structure/reach/reach_set_py_graph_online.py
@@ -80,7 +80,7 @@ class PyGraphReachableSetOnline(ReachableSet):
     @lru_cache(128)
     def drivable_area_at_step(self, step: int) -> List[ReachPolygon]:
         if step not in self._list_steps_computed:
-            util_logger.print_and_log_warning(logger, "Given step for drivable area retrieval is out of range.")
+            util_logger.print_and_log_warning(logger, f"Given step {step} for drivable area retrieval is out of range.")
             return []
 
         else:
@@ -124,13 +124,13 @@ class PyGraphReachableSetOnline(ReachableSet):
         """
         return time_index + self.step_start
 
-    @lru_cache(124)
+    @lru_cache(128)
     def reachset_translation(self, step: int) -> np.ndarray:
         """
         Translation of initial state at the given step.
         """
-        return self.config.planning.p_lon_lat_initial \
-               + self.config.planning.v_lon_lat_initial * self.config.scenario.dt * step
+        return self.config.planning.p_initial \
+               + self.config.planning.v_initial * self.config.scenario.dt * step
 
     def initialize_new_scenario(self, scenario: Optional[Scenario] = None, planning_problem: [Optional] = None):
         """

commonroad_reach/data_structure/regular_grid.py
@@ -34,8 +34,8 @@ class RegularGrid:
         self.grid_shapes = grid_shapes
 
         # filter irrelevant obstacles over complete time interval
-        init_pos = planning_config.p_lon_lat_initial
-        init_vel = planning_config.v_lon_lat_initial
+        init_pos = planning_config.p_initial
+        init_vel = planning_config.v_initial
 
         a_max = max([a_x, a_y])
         extreme_pos = np.array([init_pos + a_max * t_f ** 2 / 2 + init_vel * t_f,

commonroad_reach/utility/geometry.py
@@ -4,27 +4,27 @@ from typing import Dict, List, Tuple, Optional
 import numpy as np
 import commonroad_dc.pycrcc as pycrcc
 
-from commonroad_reach.data_structure.reach.reach_node import ReachNode
 from commonroad_reach.data_structure.reach.reach_polygon import ReachPolygon
 from commonroad_reach.data_structure.reach.reach_vertex import Vertex
 
 
-def linear_mapping(polygon, tuple_coefficients: Tuple):
-    """Returns the linear mapping of polygon.
-
-    Computing the zero-input response of the system, see Söntges T-ITS 2018, Sec. IV.A.
+def linear_mapping(polygon: ReachPolygon, tuple_coefficients: Tuple[float, float, float, float]) -> ReachPolygon:
+    """
+    Returns the linear mapping of the input polygon.
     """
     list_vertices = polygon.vertices
     a11, a12, a21, a22 = tuple_coefficients
 
-    list_vertices_mapped = [(a11 * vertex[0] + a12 * vertex[1], a21 * vertex[0] + a22 * vertex[1])
-                            for vertex in list_vertices]
+    list_vertices_mapped = [(a11 * vertex[0] + a12 * vertex[1],
+                             a21 * vertex[0] + a22 * vertex[1]) for vertex in list_vertices]
 
     return ReachPolygon(list_vertices_mapped)
 
 
 def minkowski_sum(polygon1: ReachPolygon, polygon2: ReachPolygon) -> Optional[ReachPolygon]:
-    """Returns the Minkowski sum of two polygons."""
+    """
+    Returns the Minkowski sum of the two input polygons.
+    """
     if polygon1.is_empty or polygon2.is_empty:
         return None
 
@@ -42,16 +42,14 @@ def minkowski_sum(polygon1: ReachPolygon, polygon2: ReachPolygon) -> Optional[Re
     return ReachPolygon.from_polygon(ReachPolygon(list_vertices_sum).convex_hull)
 
 
-def sort_vertices_counterclockwise(list_vertices: List[Tuple]) -> List[Tuple]:
-    """Sorts a list of vertices in the counterclockwise direction.
+def sort_vertices_counterclockwise(list_vertices: List[Tuple]) -> List[Tuple[float, float]]:
+    """
+    Sorts a list of vertices in the counterclockwise direction.
 
     Steps:
         1. compute the center of mass
-        2. compute the angles from the center of mass to each point
+        2. compute the angle from the center of mass to each point
         3. sort with the computed angles
-
-    Args:
-        list_vertices (List[Tuple]): the list of vertices to be sorted
     """
     list_x = np.array([vertex[0] for vertex in list_vertices])
     list_y = np.array([vertex[1] for vertex in list_vertices])
@@ -73,25 +71,32 @@ def sort_vertices_counterclockwise(list_vertices: List[Tuple]) -> List[Tuple]:
     return list_vertices_sorted
 
 
-def create_adjacency_dictionary(list_rectangles_1: List[ReachPolygon], list_rectangles_2: List[ReachPolygon]) \
-        -> Dict[int, List[int]]:
-    """Returns an adjacency dictionary.
+def create_adjacency_dictionary(list_rectangles_1: List[ReachPolygon],
+                                list_rectangles_2: List[ReachPolygon]) -> Dict[int, List[int]]:
+    """
+    Returns an adjacency dictionary from the two given list of rectangles.
 
-    E.g.: {0 : [1, 2], 1 : [3, 4]} = rectangle_0 from 1st list overlaps with rectangles_1 and _2 from the 2nd list;
-    rectangle_1 (1st) overlaps with rectangles_3 and _4 (2nd).
+    .. note::
+        **Example**: {0 : [1, 2], 1 : [3, 4]} = rectangle_0 from 1st list overlaps with rectangles_1 and _2 from the
+        2nd list; rectangle_1 from the 1st list overlaps with rectangles_3 and _4 from the 2nd list.
     """
     dict_idx_to_list_idx = defaultdict(list)
 
     for idx_1, rectangle_1 in enumerate(list_rectangles_1):
         for idx_2, rectangle_2 in enumerate(list_rectangles_2):
-            if rectangle_1.intersects(rectangle_2):
+            if not (rectangle_1.p_lon_min >= rectangle_2.p_lon_max or
+                    rectangle_1.p_lon_max <= rectangle_2.p_lon_min or
+                    rectangle_1.p_lat_min >= rectangle_2.p_lat_max or
+                    rectangle_1.p_lat_max <= rectangle_2.p_lat_min):
                 dict_idx_to_list_idx[idx_1].append(idx_2)
 
     return dict_idx_to_list_idx
 
 
-def obtain_extremum_coordinates_of_vertices(list_vertices):
-    """Returns the extremum coordinates of the given vertices."""
+def obtain_extremum_coordinates_of_vertices(list_vertices: List[Tuple]) -> Tuple[float, float, float, float]:
+    """
+    Returns the extremum coordinates of the given list of vertices.
+    """
     list_p_lon = [vertex[0] for vertex in list_vertices]
     list_p_lat = [vertex[1] for vertex in list_vertices]
 
@@ -103,8 +108,10 @@ def obtain_extremum_coordinates_of_vertices(list_vertices):
     return p_lon_min, p_lat_min, p_lon_max, p_lat_max
 
 
-def create_aabb_from_coordinates(p_lon_min, p_lat_min, p_lon_max, p_lat_max):
-    """Returns a pycrcc.RectAABB object from the given coordinates."""
+def create_aabb_from_coordinates(p_lon_min: float, p_lat_min: float, p_lon_max: float, p_lat_max: float):
+    """
+    Returns a pycrcc.RectAABB object from the given coordinates.
+    """
     length = p_lon_max - p_lon_min
     width = p_lat_max - p_lat_min
     center_lon = (p_lon_min + p_lon_max) / 2.0
@@ -116,7 +123,12 @@ def create_aabb_from_coordinates(p_lon_min, p_lat_min, p_lon_max, p_lat_max):
 
 
 def rectangle_intersects_with_circle(rectangle: ReachPolygon, center: Tuple[float, float], radius: float) -> bool:
-    """Returns true if the given rectangles intersects with the attributes of a circle."""
+    """
+    Returns true if the given rectangles intersects with a circle.
+
+    :param rectangle: rectangle to be checked
+    :param center: center of the circle to be checked
+    :param radius: radius of the circle to be checked"""
     x_closest = clamp(center[0], rectangle.p_lon_min, rectangle.p_lon_max)
     y_closest = clamp(center[1], rectangle.p_lat_min, rectangle.p_lat_max)
 
@@ -128,7 +140,10 @@ def rectangle_intersects_with_circle(rectangle: ReachPolygon, center: Tuple[floa
     return distance_squared < (radius * radius)
 
 
-def clamp(value, min_value, max_value):
+def clamp(value: float, min_value: float, max_value: float):
+    """
+    Clamps a value between the min and max values.
+    """
     if value <= min_value:
         return min_value
 
@@ -137,80 +152,3 @@ def clamp(value, min_value, max_value):
 
     else:
         return value
-
-
-def compute_area_of_reach_nodes(list_nodes_reach: List[ReachNode]) -> float:
-    """
-    Computes the area of a given list of reachable set nodes.
-    :param list_nodes_reach:
-    :return area: area of projection of the reachable sets on position domain
-    """
-    area = 0.0
-    if not list_nodes_reach:
-        return area
-
-    if isinstance(list_nodes_reach[0], ReachNode):
-        for node in list_nodes_reach:
-            area += (node.p_lon_max - node.p_lon_min) * (node.p_lat_max - node.p_lat_min)
-
-    else:
-        for node in list_nodes_reach:
-            area += (node.p_lon_max() - node.p_lon_min()) * (node.p_lat_max() - node.p_lat_min())
-
-    return area
-
-
-def connected_reachset_py(list_nodes_reach: List[ReachNode], num_digits: int):
-    """Determines connected sets in the position domain.
-
-    This function is the equivalent python function to pycrreach.connected_reachset_boost().
-    Returns a dictionary with key=node idx in list and value=list of tuples
-    :param list_nodes_reach: list of reachable set node
-    :param num_digits
-    """
-    coefficient = np.power(10.0, num_digits)
-    dict_adjacency = defaultdict(list)
-
-    # list of drivable areas (i.e., position rectangles)
-    list_position_rectangles = list()
-
-    # preprocess
-    for node_reach in list_nodes_reach:
-        # enlarge position rectangles
-        vertices_rectangle_scaled = (np.floor(node_reach.p_lon_min * coefficient),
-                                     np.floor(node_reach.p_lat_min * coefficient),
-                                     np.ceil(node_reach.p_lon_max * coefficient),
-                                     np.ceil(node_reach.p_lat_max * coefficient))
-        list_position_rectangles.append(ReachPolygon.from_rectangle_vertices(*vertices_rectangle_scaled))
-
-    # iterate over all rectangles in list
-    for idx1, position_rect_1 in enumerate(list_position_rectangles):
-        for idx2, position_rect_2 in enumerate(list_position_rectangles):
-            if idx1 == idx2:
-                continue
-
-            # check for dict_adjacency via shapely intersects() function. If True, add tuple of idx to dict
-            if position_rect_1.intersects(position_rect_2):
-                dict_adjacency[idx1].append((idx1, idx2))
-
-    return dict_adjacency
-
-
-def lon_interval_connected_set(connected_set):
-    """Projects a connected set onto longitudinal position domain and returns min/max longitudinal positions"""
-    # get min and max values for each reachable set in the connected set
-    min_max_array = np.asarray([[reach_node.p_lon_min(), reach_node.p_lon_max()] for reach_node in connected_set])
-    # get minimum and maximum value for the connected set
-    min_connected_set = np.min(min_max_array[:, 0])
-    max_connected_set = np.max(min_max_array[:, 1])
-    return min_connected_set, max_connected_set
-
-
-def lat_interval_connected_set(connected_set):
-    """Projects a connected set onto lateral position domain and returns min/max lateral positions"""
-    # get min and max values for each reachable set in the connected set
-    min_max_array = np.asarray([[reach_node.p_lat_min(), reach_node.p_lat_max()] for reach_node in connected_set])
-    # get minimum and maximum value for the connected set
-    min_connected_set = np.min(min_max_array[:, 0])
-    max_connected_set = np.max(min_max_array[:, 1])
-    return min_connected_set, max_connected_set