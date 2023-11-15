e0ee462c19eef5c11ac3bf56c62d041f43d1bc26
Florian Lercher <florian.lercher@tum.de>
2023-11-15 18:44:37 +0100

    Update pybind version

--- cmake/external/ExternalPybind.cmake
+++ cmake/external/ExternalPybind.cmake
@@ -2,17 +2,17 @@ if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.24)
     FetchContent_Declare(
             pybind11
 
-            URL            https://github.com/pybind/pybind11/archive/refs/tags/v2.10.4.tar.gz
-            URL_HASH       SHA256=832e2f309c57da9c1e6d4542dedd34b24e4192ecb4d62f6f4866a737454c9970
+            URL            https://github.com/pybind/pybind11/archive/refs/tags/v2.11.1.tar.gz
+            URL_HASH       SHA256=d475978da0cdc2d43b73f30910786759d593a9d8ee05b1b6846d1eb16c6d2e0c
 
-            FIND_PACKAGE_ARGS 2.10.4
+            FIND_PACKAGE_ARGS 2.11.1
     )
 else()
     FetchContent_Declare(
             pybind11
 
-            URL            https://github.com/pybind/pybind11/archive/refs/tags/v2.10.4.tar.gz
-            URL_HASH       SHA256=832e2f309c57da9c1e6d4542dedd34b24e4192ecb4d62f6f4866a737454c9970
+            URL            https://github.com/pybind/pybind11/archive/refs/tags/v2.11.1.tar.gz
+            URL_HASH       SHA256=d475978da0cdc2d43b73f30910786759d593a9d8ee05b1b6846d1eb16c6d2e0c
     )
 endif()
 FetchContent_MakeAvailable(pybind11)
